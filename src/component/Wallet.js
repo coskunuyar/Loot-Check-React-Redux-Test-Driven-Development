6db008771fc463bad7b09f55e6aaa89d9b22c33a
Coskun Uyar <coskunuyarr@gmail.com>
2020-03-28 21:22:37 +0300

    Deposit and withdraw functionality.

--- src/component/Wallet.js
+++ src/component/Wallet.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
+import { deposit , withdraw} from '../actions/balance';
 
 export class Wallet extends Component{
     constructor(props){
@@ -11,12 +12,17 @@ export class Wallet extends Component{
 
     updateBalance = event => this.setState({ balance: parseInt(event.target.value,10) })
 
+    deposit = () => this.props.deposit(this.state.balance);
+    withdraw = () => this.props.withdraw(this.state.balance);
+
     render(){
         return(
             <div>
                 <h3 className="balance">Wallet Balance: {this.props.balance}</h3>
                 <br />
                 <input className="input-wallet" onChange={this.updateBalance} />
+                <button className="btn-deposit" onClick={this.deposit}>Deposit</button>
+                <button className="btn-withdraw" onClick={this.withdraw}>Withdraw</button>
             </div>
         );
     }
@@ -24,4 +30,4 @@ export class Wallet extends Component{
 
 
 
-export default connect(state => ({ balance: state }),null)(Wallet);
+export default connect(state => ({ balance: state }),{ deposit , withdraw })(Wallet);
--- src/component/Wallet.test.js
+++ src/component/Wallet.test.js
@@ -6,7 +6,9 @@ import {shallow , configure} from 'enzyme';
 configure({adapter: new Adapter()});
 
 describe('Wallet',() => {
-    const props = { balance: 20 };
+    const mockDeposit = jest.fn();
+    const mockWithdraw= jest.fn();
+    const props = { balance: 20 , deposit: mockDeposit , withdraw: mockWithdraw };
     const wallet = shallow(<Wallet {...props}/>);
     it('renders properly',() => {
         expect(wallet).toMatchSnapshot();
@@ -25,8 +27,23 @@ describe('Wallet',() => {
         beforeEach(() => {
             wallet.find('.input-wallet').simulate('change', { target: { value: userBalance }});
         });
+
         it('updates the local wallet balance in the state and coverts into number',() => {
             expect(wallet.state().balance).toEqual(parseInt(userBalance,10));
+        });
+
+        describe('and the user wants to make a deposit',() => {
+            beforeEach(() => wallet.find('.btn-deposit').simulate('click'));
+            it('dispatches the deposit with local balance', () => {
+                expect(mockDeposit).toHaveBeenCalledWith(parseInt(userBalance,10));
+            });
+        });
+
+        describe('and the user wants to make a withdraw',() => {
+            beforeEach(() => wallet.find('.btn-withdraw').simulate('click'));
+            it('dispatches the withdraw with local balance', () => {
+                expect(mockDeposit).toHaveBeenCalledWith(parseInt(userBalance,10));
+            });
         });
     });
 });
