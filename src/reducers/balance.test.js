0a58c3e908f890d7401ee4393f76f50ac3b5d1e1
Coskun Uyar <coskunuyarr@gmail.com>
2020-03-28 20:22:17 +0300

    Deposit action and reducer is added.

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -7,4 +7,12 @@ describe('balanceReducer',() => {
         expect(balanceReducer(undefined,{ 
             type: constants.SET_BALANCE , balance })).toEqual(balance);        
     });
+
+    it('deposit into the balance',() => {
+        const deposit = 10;
+        const initialState = 5;
+        expect(balanceReducer(initialState,{
+            type: constants.DEPOSIT , deposit
+        })).toEqual(initialState + deposit);
+    });
 });
