3e64ef7178a91f1823cdb7c44fa350414a98c7ba
Coskun Uyar <coskunuyarr@gmail.com>
2020-03-28 20:44:10 +0300

    local balance input and state is added.

--- src/component/Wallet.test.js
+++ src/component/Wallet.test.js
@@ -15,4 +15,18 @@ describe('Wallet',() => {
     it('displays the balance form props',() => {
         expect(wallet.find('.balance').text()).toEqual('Wallet Balance: 20')
     });
+
+    it('creates an input to deposit into or withdraw from the balance',() => {
+        expect(wallet.find('.input-wallet').exists()).toBe(true);
+    });
+
+    describe('when the user type into the wallet input',() => {
+        const userBalance = '25';
+        beforeEach(() => {
+            wallet.find('.input-wallet').simulate('change', { target: { value: userBalance }});
+        });
+        it('updates the local wallet balance in the state and coverts into number',() => {
+            expect(wallet.state().balance).toEqual(parseInt(userBalance,10));
+        });
+    });
 });
