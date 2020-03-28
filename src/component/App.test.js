d676ee7e5bb11aafe374ed9b5915752de64662c3
Coskun Uyar <coskunuyarr@gmail.com>
2020-03-28 19:32:24 +0300

    setBalance action and test is added

--- src/component/App.test.js
+++ src/component/App.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+import {shallow , configure} from 'enzyme';
+
+configure({adapter: new Adapter()});
 
 describe('App',() => {
     const app = shallow(<App/>);
     
     it('renders properly',() => {
-        expect(app).toBeMatchSnapshot();
+        expect(app).toMatchSnapshot();
     });
 });
